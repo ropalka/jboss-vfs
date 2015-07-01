c8436535f4ef01564edeb434e8061893192823db
Richard Opalka <opalka.richard@gmail.com>
2015-07-01 22:13:12 +0200

    Removing useless VFSUtils.getGlobPattern() method

--- src/main/java/org/jboss/vfs/VFSUtils.java
+++ src/main/java/org/jboss/vfs/VFSUtils.java
@@ -44,8 +44,6 @@ import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -762,65 +760,6 @@ public class VFSUtils {
     public static File getMountSource(Closeable handle) {
         if (handle instanceof MountHandle) { return MountHandle.class.cast(handle).getMountSource(); }
         return null;
-    }
-
-    private static final Pattern GLOB_PATTERN = Pattern.compile("(\\*\\*?)|(\\?)|(\\\\.)|(/+)|([^*?]+)");
-
-    /**
-     * Get a regular expression pattern which matches any path names which match the given glob.  The glob patterns
-     * function similarly to {@code ant} file patterns.  Valid meta-characters in the glob pattern include:
-     * <ul>
-     * <li><code>"\"</code> - escape the next character (treat it literally, even if it is itself a recognized meta-character)</li>
-     * <li><code>"?"</code> - match any non-slash character</li>
-     * <li><code>"*"</code> - match zero or more non-slash characters</li>
-     * <li><code>"**"</code> - match zero or more characters, including slashes</li>
-     * <li><code>"/"</code> - match one or more slash characters.  Consecutive {@code /} characters are collapsed down into one.</li>
-     * </ul>
-     * In addition, like {@code ant}, if the pattern ends with a {@code /}, then an implicit <code>"**"</code> will be appended.
-     * <p/>
-     * <b>See also:</b> <a href="http://ant.apache.org/manual/dirtasks.html#patterns">"Patterns" in the Ant Manual</a>
-     *
-     * @param glob the glob to match
-     * @return the pattern
-     */
-    public static Pattern getGlobPattern(final String glob) {
-        StringBuilder patternBuilder = new StringBuilder();
-        patternBuilder.append("^");
-        final Matcher m = GLOB_PATTERN.matcher(glob);
-        boolean lastWasSlash = false;
-        while (m.find()) {
-            lastWasSlash = false;
-            String grp;
-            if ((grp = m.group(1)) != null) {
-                // match a * or **
-                if (grp.length() == 2) {
-                    // it's a **
-                    patternBuilder.append(".*");
-                } else {
-                    // it's a *
-                    patternBuilder.append("[^/]*");
-                }
-            } else if ((grp = m.group(2)) != null) {
-                // match a '?' glob pattern; any non-slash character
-                patternBuilder.append("[^/]");
-            } else if ((grp = m.group(3)) != null) {
-                // backslash-escaped value
-                patternBuilder.append(grp.charAt(1));
-            } else if ((grp = m.group(4)) != null) {
-                // match any number of / chars
-                patternBuilder.append("/+");
-                lastWasSlash = true;
-            } else {
-                // some other string
-                patternBuilder.append(Pattern.quote(m.group()));
-            }
-        }
-        if (lastWasSlash) {
-            // ends in /, append **
-            patternBuilder.append(".*");
-        }
-        patternBuilder.append("$");
-        return Pattern.compile(patternBuilder.toString());
     }
 
     /**
